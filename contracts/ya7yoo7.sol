0b08c14dda8bcd6ea4673af702e72e360cdbeb6e
yehia67 <yehiatarek67@gmail.com>
2019-11-18 01:07:11 +0200

    some bugs on token sales

--- contracts/ya7yoo7.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.4.21 <0.6.0;
-import "./IERC20.sol";
-contract ya7yoo7 {
-    string  public name = "YA7YOU7";
-    string  public symbol = "YA7";
-    string  public standard = "YA7YOU& Token v1.0";
-}
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var ya7you7 = artifacts.require("./ya7you7.sol");
+
+module.exports = function(deployer) {
+    deployer.deploy(ya7you7);
+};
